a1e4057be568223a40003d460ac4f7a609e5b2cd
Laura Trager <laura.trager@gmail.com>
2017-11-20 14:48:45 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,21 +3,23 @@ class EmailParser
 
   attr_accessor :email_set
 
-  @@email_array = []
+  @email_array = []
 
   def self.all
     @@email_array
   end
 
   def initialize(emails)
-    @@email_array << emails
+    @email_array << emails
   end
 
   def parse
-    @@email_array.join(" ").gsub(" ", ",").split.join.gsub(",,", ",").split(",").uniq
+    @email_array.join(" ").gsub(" ", ",").split.join.gsub(",,", ",").split(",").uniq
   end
 
 end
 
-EmailParser.new("avi@test.com, avi@test.com").parse
+EmailParser.new("avi@test.com, otheravi@test.com").parse
+
+#EmailParser.new("avi@test.com, avi@test.com").parse
 #=> ["avi@test.com"]
