7963fc1237e24755639da012ae6e77792f5aa0cd
Laura Trager <laura.trager@gmail.com>
2017-11-20 13:47:26 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,46 +1,20 @@
-require 'pry'
 
 class EmailParser
 
   attr_accessor :email_set
 
-  @@all =[]
+  @@email_array = []
 
   def self.all
-    @@all
+    @@email_array
   end
 
-  def initialize(email_set)
-    @@all << email_set
+  def initialize(emails)
+    @@email_array << emails
   end
 
   def parse
-    #string = @@all.join
-
-    #if @@all.join.include?("," && " ")
-      #@@all.join.gsub.split
-
-    #elsif @@all.join.include?(", ")
-      #@@all.split(",")
-
-    #elsif @@all.join.include?(",") == false
-      #@@all.split(",")
-
-      # works for COMBO, takes in array (need to join then split then join?)
-   print @@emails[0].join.split(", ").join(" ").split
-
-   #print @@emails.join.split(", ") # works for COMMAS | take in string
-
-   #works for SPACES | take in string
-   #print @@emails.join.split
-
-    #end
+    @@email_array.join(" ").gsub(" ", ",").split.join.gsub(",,", ",").split(",")
   end
-
-  #to_parse = EmailParser.new("avi@test.com arel@test.com test@avi.com test@arel.com")
-
-  #to_parse.parse
-
-  #binding.pry
 
 end
